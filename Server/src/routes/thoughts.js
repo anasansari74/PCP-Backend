873da1c0d61ab640fdcb49a51fb6141b4272ef21
Geeta Kotecha <geeta.kotecha@gmail.com>
2022-01-17 16:32:09 +0000

    stuck on fetch by id

--- Server/src/routes/thoughts.js
+++ Server/src/routes/thoughts.js
@@ -11,4 +11,16 @@ thoughtsRouter.route("/thoughts").get(async function (req, res) {
   await db_connect.collection("thoughts").find({}).toArray();
 });
 
+thoughtsRouter.route("/thoughts/:id").get(async function (req, res) {
+  let db_connect = dbo.getDb("positive-thought-generator");
+  let myquery = { _id: ObjectId(`${req.params.id}`)};
+  await db_connect
+    .collection("positiveThoughtGenerator.positiveThoughts")
+    .find(myquery)
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
 module.exports = thoughtsRouter;
