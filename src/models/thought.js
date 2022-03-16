b9cd2f2b740e378bda0b89f00c9d2a9e6e7215c1
Geeta Kotecha <geeta.kotecha@gmail.com>
2022-03-16 15:18:05 +0000

    got data from api

--- src/models/thought.js
+++ src/models/thought.js
@@ -1,4 +1,40 @@
 const mongoose = require("mongoose");
+const axios = require('axios');
+
+let categories = []
+
+
+const getTags = () => {
+  try {
+    return axios.get('https://goquotes-api.herokuapp.com/api/v1/all/tags')
+    .then(response => {
+      if (response.data.tags) {
+        console.log(
+          `Got ${Object.entries(response.data.tags)} tags`
+        )
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// .then(response => {
+//   if (response.data.message) {
+//     console.log(
+//       `Got ${Object.entries(response.data.message).length} breeds`
+//     )
+//   }
+// })
+
+
+
+
+getTags()
+
+console.log("categories", categories)
+
+
 
 const ThoughtSchema = new mongoose.Schema({
   categoryTitle: {
